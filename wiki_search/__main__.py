396a823f01c10ca6b41c490353da819f2bab595c
Ilya Savitsky <ipsavitsky234@gmail.com>
2023-05-03 16:51:06 +0300

    added locale and table switches

--- wiki_search/__main__.py
+++ wiki_search/__main__.py
@@ -5,22 +5,32 @@ from rich.console import Console
 from rich.markdown import Markdown
 from soup_filters import filter_soup
 from wikipedia import NoArticleFound, fetch_wiki_data
+from options import Options
+import logging as log
+
+log.basicConfig(level=log.INFO)
 
 console = Console()
+options = Options()
 
 parser = argparse.ArgumentParser(description="Search a Wikipedia article and print it")
+parser.add_argument("--locale", type=str, default="en")
+parser.add_argument("--skip_tables", action="store_false")
 parser.add_argument("keyword", type=str, help="The keyword to search")
 args = parser.parse_args()
 
+options.class_prefix = args.locale
+options.skip_tables = args.skip_tables
+
 try:
-    page_info = fetch_wiki_data(args.keyword)
+    page_info = fetch_wiki_data(options, args.keyword)
 except NoArticleFound as e:
     console.print(f'No article "{e.fprompt}" found')
     exit(1)
 
 soup = BeautifulSoup(page_info["text"]["*"], "html.parser")
 
-filtered_soup = filter_soup(page_info["title"], soup)
+filtered_soup = filter_soup(options, page_info["title"], soup)
 
 md = MarkdownConverter(escape_underscores=False, autolinks=False).convert_soup(
     filtered_soup
